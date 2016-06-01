c601a35e99876c9baf55699f391df3070a8b5406
ChenL1994 <1570184051@qq.com>
2016-06-01 06:48:00 -0500

    Add files via upload

---
+++ MaxNumber.java
@@ -0,0 +1,16 @@
+public class MaxNumber {
+
+	static int max(int a,int b){
+		
+		return a*(b/a)+b*(a/b)+1;//这种方法必须要a,b不能为倍数
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2;i<=100;i++){
+			if(max(i,i+1)<i||max(i+1,i)<i){
+				System.out.println("错误");
+			}
+		}
+	}
+
+}
