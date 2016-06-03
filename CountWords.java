fe01928183bc3f1af4ab7b24ac8800408b14ad8d
TyranitarX <tyranitarx@163.com>
2016-06-03 08:34:09 +0800

    第三周任务

---
+++ CountWords.java
@@ -0,0 +1,13 @@
+﻿
+public class CountWords {
+	public int Count(String[] article,int n,String word)
+	{
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			if(article[i].equals(word))
+				count++;
+		}
+		return count;
+	}
+}
---
+++ UnusualAdd.java
@@ -0,0 +1,15 @@
+﻿
+public class UnusualAdd {
+	public int AddAB(int a ,int b)
+	{
+		if(b==0)
+			return a;
+		else
+			return AddAB(a^b,(a&b)<<1);
+	}
+	public static void main(String[] args)
+	{
+		UnusualAdd ua=new UnusualAdd();
+		System.out.println(ua.AddAB(1, 2));
+	}
+}
