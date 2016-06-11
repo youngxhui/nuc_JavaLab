068996fd0bf014e3cfc0409686feebba4deec389
TyranitarX <tyranitarx@163.com>
2016-06-11 22:52:04 +0800

    第四周作业

---
+++ Coins.java
@@ -0,0 +1,28 @@
+﻿//1,5,20,25;
+public class Coins {
+	int CountWays(int n,int coin)  
+	{  
+	    int next=0;  
+	    switch(coin)  
+	    {  
+	    case 25:  
+	        next=10;  
+	        break;  
+	    case 10:  
+	        next=5;  
+	        break;  
+	    case 5:  
+	        next=1;  
+	        break;  
+	    case 1:  
+	        return 1;  
+	    }  
+	  
+	    int count=0;  
+	    for(int i=0;i*coin<=n;i++)  
+	        count+=CountWays(n-i*coin,next);  
+	  
+	    return count;  
+	  
+	}  
+}
---
+++ Count2.java
@@ -0,0 +1,18 @@
+﻿public class Count2 {
+	public int countNumberOf2s(int n)
+	{
+		int x=0;
+		for(int i=0;i<=n;i++)
+		{
+			String s=Integer.toString(i);
+			char[] str=s.toCharArray();
+			for(int j=0;j<str.length;j++)
+			{
+				
+				if(str[j]=='2')
+					x++;
+			}
+		}
+		return x;
+	}
+}
