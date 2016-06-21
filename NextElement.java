e8973cf10e5c53f9332916edfcb60d96c203c1cb
TyranitarX <tyranitarx@163.com>
2016-06-21 16:50:01 +0800

    第五周作业

---
+++ NextElement.java
@@ -0,0 +1,22 @@
+﻿public class NextElement {
+	public int[] findNext(int[] A,int B)
+	{
+		int temp=0;
+		for(int i=0;i<B;i++)
+		{
+			temp=A[i];
+			for(int j=i;j<B;j++)
+			{
+				if(A[j]>A[i])
+				{
+					A[i]=A[j];
+					break;
+				}
+				
+			}
+			if(A[i]==temp)
+				A[i]=-1;
+		}
+		return A;
+	}
+}
---
+++ Robot.java
@@ -0,0 +1,21 @@
+﻿public class Robot {
+	public int countWays(int x ,int y)
+	{
+		int A[][]=new int[x][y];
+		for(int i=0;i<x;i++)
+		{
+			for(int j=0;j<y;j++)
+			{
+				if(i-1<0&&j-1<0)
+					A[i][j]=1;
+				else if(i-1<0)
+					A[i][j]=A[i][j-1];
+				else if(j-1<0)
+					A[i][j]=A[i-1][j];
+				else
+				A[i][j]=A[i-1][j]+A[i][j-1];
+			}
+		}
+		return A[x-1][y-1];
+	}
+}
