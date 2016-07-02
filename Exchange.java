adaeb128db22a758ccda28b2fd0e048c8f06851d
ChenL1994 <1570184051@qq.com>
2016-07-02 13:21:00 +0800

    Create Exchange.java

---
+++ Exchange.java
@@ -0,0 +1,20 @@
+public class Exchange {
+
+	public int[] exchangeAB(int[] AB) {
+        // write code here
+		AB[1]=AB[1]+AB[0];
+		AB[0]=AB[1]-AB[0];
+		AB[1]=AB[1]-AB[0];
+		return AB;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exchange e=new Exchange();
+		int[] s={1,2,3};
+		int[] t=e.exchangeAB(s);
+		for(int i : t){
+			System.out.println(i);
+		}
+	}
+
+}
