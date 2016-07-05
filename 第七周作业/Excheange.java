f75062782ddf24afaf8b850005b4ec727c428c47
TyranitarX <tyranitarx@163.com>
2016-07-05 10:02:37 +0800

    任务

---
+++ 第七周作业/Excheange.java
@@ -0,0 +1,11 @@
+﻿import java.util.Scanner;
+public class Exchange {
+	public int[] exchange(int [] AB)
+	{
+		AB[0]=AB[0]+AB[1];
+		AB[1]=AB[0]-AB[1];
+		AB[0]=AB[0]-AB[1];
+		return AB;
+	}
+
+}
---
+++ 第七周作业/Transform.java
@@ -0,0 +1,30 @@
+﻿
+public class Transform {
+	public int calcCost(int a ,int b)
+	{
+		int count=0;
+		String temp;
+		String x=Integer.toBinaryString(a);
+		String y=Integer.toBinaryString(b);
+		if(x.length()<y.length())
+		{
+			temp=x;
+			x=y;
+			y=temp;
+		}
+		for(int i=0;i<x.length()-y.length();i++)
+		{
+			y="k"+y; 
+			
+		}
+		for(int i=0;i<x.length();i++)
+		{
+			if(x.charAt(i)!=y.charAt(i))
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+}
