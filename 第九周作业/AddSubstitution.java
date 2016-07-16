12d3f287691d4c08c93dd39d13c117793c6566fb
TyranitarX <tyranitarx@163.com>
2016-07-16 10:29:48 +0800

    任务

---
+++ 第九周作业/AddSubstitution.java
@@ -0,0 +1,31 @@
+﻿public class AddSubstitution {
+	public int calc(int a,int b,int type)
+	{
+		int cal=0;
+		switch(type)
+		{
+		case 1:
+			for(int i=0;i<b;i++)
+			{
+				cal=cal+a;
+			}
+			return cal;
+		case 0:
+			for(int i=0;;i++)
+			{
+				if(a<=0)
+					break;
+				else
+				{
+					a=calc(a,b,-1);
+					cal++;
+				}
+			}
+			return cal;
+		case -1:
+			return a+~b+1;
+		
+		}
+		return cal;
+	}
+}
---
+++ 第九周作业/CrossLine.java
@@ -0,0 +1,10 @@
+﻿public class CrossLine {
+	public boolean checkCrossLine(double s1,double s2,double y1,double y2)
+	{
+		if(s1==y1&&s2!=y2)
+		{
+			return false;
+		}
+		return true;
+	}
+}
