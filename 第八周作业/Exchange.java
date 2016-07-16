3c414a056e7cb55e7bd175c38cb7cf02efe9896e
TyranitarX <tyranitarx@163.com>
2016-07-16 08:57:26 +0800

    任务

---
+++ 第八周作业/Exchange.java
@@ -0,0 +1,25 @@
+﻿public class Exchange {
+	public int exchangeOddEven(int x)
+	{
+		String x1=Integer.toBinaryString(x);	
+		String[] x2=x1.split("");
+		String temp=null;
+		for(int i=x2.length-1;i>=0;i--)
+		{
+			if((i+1)%2==0)
+			{
+				
+				temp=x2[i];
+				x2[i]=x2[i-1];
+				x2[i-1]=temp;
+			}
+		}
+		int k=0,x3=0;
+		for(int i=x2.length-1;i>=0;i--)
+		{
+			x3=(int) (x3+Integer.parseInt(x2[i])*Math.pow(2,k));
+			k++;
+		}
+			return x3;
+	}
+}
---
+++ 第八周作业/Replacement.java
@@ -0,0 +1,19 @@
+﻿public class Replacement {
+	public String ReplaceSpace(String initString ,int length)
+	{
+		String init1[]=initString.split("");
+		String init2="";
+		for(int i=0;i<length;i++)
+		{
+			if(init1[i].equals(" "))
+			{
+				init1[i]="%20";
+			}
+		}
+		for(int i=0;i<init1.length;i++)
+		{
+			init2=init2+init1[i];
+		}
+		return init2;
+	}
+}
