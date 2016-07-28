a771eeea093a686f8034deb7603294da759012f2
TyranitarX <tyranitarx@163.com>
2016-07-28 11:02:56 +0800

    任务

--- 第十周作业/Solution.java
+++ 第十周作业/Solution.java
@@ -1,9 +1,9 @@
 ﻿public class Solution{
-	public long jumpFloor(int n) {
+	public long jumpFloorII(int n) {
     	if (n <= 0)
         	return -1;
     	if (n == 1)
         	return 1;
-    	return 2 * jumpFloor(n - 1);
+    	return 2 * jumpFloorII(n - 1);
 	}
 }
