ba9207f23c6a9ad07d0e4dd2027b1e4961252c8b
wean <438864121@qq.com>
2016-07-31 20:09:05 +0800

    第十周作业

---
+++ 第十周/Solution (2).java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int Sum_Solution(int n) {
+         int sum = n;
+        boolean ans = (n>0)&&((sum+=Sum_Solution(n-1))>0);
+        return sum;
+        
+    }
+}
