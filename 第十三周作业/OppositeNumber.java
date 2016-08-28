98d050ec69119bb2e313284d3f851aa9abf2be3d
TyranitarX <tyranitarx@163.com>
2016-08-28 12:03:35 +0800

    第十三周作业

---
+++ 第十三周作业/OppositeNumber.java
@@ -0,0 +1,24 @@
+﻿import  java.util.*;
+public class OppositeNumber {
+	public int CountNum(){
+		Scanner in=new Scanner(System.in);
+		int num,count=0;
+		num=in.nextInt();
+		int[] a=new int[501];
+		int [] Num=new int[num];
+		for(int i=0;i<Num.length;i++){
+			Num[i]=in.nextInt();
+			}
+		for(int i=1;i<Num.length;i++){
+			a[Math.abs(Num[i])]++;
+		}
+		for(int i=1;i<501;i++){
+			if(a[i]>1){
+				
+				count++;
+			}
+		}
+		return count;
+	}
+	
+}
---
+++ 第十三周作业/Window.java
@@ -0,0 +1,59 @@
+﻿import java.util.*;
+
+public class Window {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int m = input.nextInt();
+		int[][] values = new int[n][4];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 4; j++) {
+				values[i][j] = input.nextInt();
+			}
+		}
+
+		int[] num = new int[n];
+		for (int i = 1; i <= n; i++) {
+			num[i - 1] = i;
+		}
+
+		String[] results = new String[m];
+		for (int i = 0; i < m; i++) {
+			int x = input.nextInt();
+			int y = input.nextInt();
+			for (int j = 0; j < n; j++) {
+				if (x >= values[j][0] && x <= values[j][2] && y >= values[j][1] && y <= values[j][3]) {
+					if (results[i] == null || num[Integer.parseInt(results[i])] < num[j]) {
+						results[i] = String.valueOf(j);
+
+					}
+				}
+			}
+
+			int top = 0;
+			for (int k = 0; k < n; k++) {
+				if (num[k] == n) {
+					top = k;
+				}
+			}
+			if (results[i] == null) {
+				results[i] = "IGNORED";
+			} else {
+				int temp = num[Integer.parseInt(results[i])];
+				num[Integer.parseInt(results[i])] = n;
+				num[top] = temp;
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (results[i] != null) {
+				results[i] = String.valueOf(Integer.parseInt(results[i]) + 1);
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			System.out.println(results[i]);
+		}
+
+	}
+}
