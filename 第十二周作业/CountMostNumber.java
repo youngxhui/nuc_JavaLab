3c45f9b93d57dbac5bcd83101c64b48d26c1cd21
TyranitarX <tyranitarx@163.com>
2016-08-28 10:16:34 +0800

    第十二周作业

---
+++ 第十二周作业/CountMostNumber.java
@@ -0,0 +1,26 @@
+﻿import java.util.*;
+public class CountMostNumber {
+	public int getMost(){
+		int[] CountNum=new int[10010];
+		int kind,max,Most = 0,num;
+		Scanner in=new Scanner(System.in);
+		num=in.nextInt();
+		for(int i=0;i<num;i++){
+			kind=in.nextInt();
+			CountNum[kind]++;
+		}
+		max=0;
+		for(int i=0;i<10000;i++){
+			if(CountNum[i]>max){
+				max=CountNum[i];
+				Most=i;
+			}
+			else if(CountNum[i]==max){
+				if(i<Most){
+					Most=i;
+				}
+			}
+		}
+		return Most;
+	}
+}
---
+++ 第十二周作业/ISBN.java
@@ -0,0 +1,34 @@
+﻿import java.util.*;
+public class ISBN {
+	public String JudgeISBN(){
+		Scanner in=new Scanner(System.in);
+		String ISBN;
+		int sum=0;
+		char num2[] = new char[3];
+		char num3[] = new char[5];
+		ISBN=in.next();
+		String[] NUM=ISBN.split("-");
+		sum=sum+Integer.parseInt(NUM[0]);
+		for(int i=0;i<3;i++){
+			num2[i]=NUM[1].charAt(i);
+		}
+		for(int i=0;i<5;i++){
+			num3[i]=NUM[2].charAt(i);
+		}
+		sum=sum+Integer.parseInt(num2[0]+"")*2+Integer.parseInt(num2[1]+"")*3+Integer.parseInt(num2[2]+"")*4+Integer.parseInt(num3[0]+"")*5+Integer.parseInt(num3[1]+"")*6+Integer.parseInt(num3[2]+"")*7+Integer.parseInt(num3[3]+"")*8+Integer.parseInt(num3[4]+"")*9;
+		sum=sum%11;
+		if(Integer.parseInt(NUM[3])==sum)
+			return "Right";
+		else if(sum==10){
+			if(NUM[3].equals("X"))
+				return "Right";
+			else
+				NUM[3]="X";
+		}
+		else {
+			NUM[3]=sum+"";
+			
+		}
+		return NUM[0]+"-"+NUM[1]+"-"+NUM[2]+"-"+NUM[3];
+	}
+}
