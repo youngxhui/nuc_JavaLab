ae9fc2cbbbbf9085ebfe2c839377b4baa5e16fcf
TyranitarX <tyranitarx@163.com>
2016-09-09 21:23:01 +0800

    第十五周作业

---
+++ 第十五周作业/LibraryManagerment.java
@@ -0,0 +1,18 @@
+﻿import java.util.Scanner;
+public class LibraryManagerment {
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		int count[]=new int[9999];
+		int people[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			people[i]=in.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			System.out.println(++count[people[i]]+" ");
+		}
+	}
+}
---
+++ 第十五周作业/Zcount.java
@@ -0,0 +1,34 @@
+﻿import java.util.Scanner;
+
+public class Zcount {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int k;
+		k = in.nextInt();
+		int form[][] = new int[k][k];
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < k; j++) {
+				form[i][j] = in.nextInt();
+			}
+		}
+		for (int count = 0; count <=2*(k-1); count++) {
+			if ((count%2) != 0) {
+				for (int i = 0; i < k; i++) {
+					for (int j = 0; j < k; j++) {
+						if (i + j == count)
+
+							System.out.print(form[i][j]+" ");
+
+					}
+				}
+			} else {
+				for (int i = k - 1; i >= 0; i--) {
+					for (int j = k - 1; j >= 0; j--) {
+						if (i + j == count)
+							System.out.print(form[i][j]+" ");
+					}
+				}
+			}
+		}
+	}
+}
