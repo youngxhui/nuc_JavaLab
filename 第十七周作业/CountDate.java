031662ad979b04445a1e86b33e0c8d3ec68cb910
TyranitarX <tyranitarx@163.com>
2016-09-27 17:10:21 +0800

    第十七周作业

---
+++ 第十七周作业/CountDate.java
@@ -0,0 +1,37 @@
+﻿import java.util.Scanner;
+public class CountDate {
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int y,d;
+		int monthr[]={31,29,31,30,31,30,31,31,30,31,30,31};
+		int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+		int month1 = 0;
+		y=in.nextInt();
+		d=in.nextInt();
+		if(y%4==0&&y%100!=0||y%400==0){
+			for(int i=0;i<monthr.length;i++){
+				if(d>=monthr[i])
+				d=d-monthr[i];
+				if(d<monthr[i+1])
+				{
+					month1=i+2;
+					break;
+				}
+			}
+		}
+		else
+		{
+			for(int i=0;i<month.length;i++){
+				if(d>=month[i])
+				d=d-month[i];
+				if(d<month[i+1])
+				{
+					month1=i+2;
+					break;
+				}
+			}
+		}
+		System.out.println(month1);
+		System.out.println(d);
+	}
+}
---
+++ 第十七周作业/NumberList.java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+public class NumberList {
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		int n;
+		n=in.nextInt();
+		int a,b;
+		int c=1;
+		int NumberList[]=new int[n];
+		for(int i=0;i<n;i++){
+			NumberList[i]=in.nextInt();
+		}
+		for(int i=1;i<n;i++){
+			a=NumberList[i-1];
+			b=NumberList[i];
+			if(a!=b)
+				c++;
+		}
+		System.out.println(c);
+	}
+}
