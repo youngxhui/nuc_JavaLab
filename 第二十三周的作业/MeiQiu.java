ed7f3052243350b6329eede3f1b7173023f1553d
高浩东 <2633576815@qq.com>
2016-12-04 20:00:19 +0800

    煤球
    煤球问题

---
+++ 第二十三周的作业/MeiQiu.java
@@ -0,0 +1,11 @@
+class MeiQiu{
+	public class void main(String [] args){
+		int a = 2,n=1;
+		for(int i = 2;i<=100;i++)
+		{
+			n =n+a;
+			a++;
+		}
+		System.out.println("100²ãµÄÃºÇòÊýÎª£º"+n);
+	}
+}
